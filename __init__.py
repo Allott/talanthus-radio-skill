080ed9aeabbcdb2b02b787bca7da5bda79a78298
Daniel Allott <36487931+Allott@users.noreply.github.com>
2020-04-07 18:14:46 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ class TalanthusRadio(MycroftSkill):
         client.run_in_thread()
 
         print('Sending speak message...')
-        client.emit(Message('speak', data={'utterance': 'Currently at the top of the bounty board is the calamo five at 1000000 credits'}))
+        client.emit(Message('speak', data={'utterance': 'Currently at the top of the bounty board is the calamo five at one million credits each'}))
         
 def create_skill():
     return TalanthusRadio()
